bc7168732131efe7b69dba3adbad36b76d87dc20
Junghoon Lee <jeonghunn1@gmail.com>
2021-05-19 14:29:29 +0900

    chore: Fix front test

--- cypress/integration/app/layout/navbar/navbar.component.spec.js
+++ cypress/integration/app/layout/navbar/navbar.component.spec.js
@@ -8,7 +8,7 @@ context('Navigation Bar Component', () => {
 
     it('Sign out', () => {
       cy.signin('user');
-      cy.get('app-navbar').find('button').contains('Test').click();
+      cy.get('app-navbar').find('.mat-focus-indicator > .mat-button-wrapper > .mat-icon').last().click();
       cy.get('.mat-menu-content').contains('Sign Out').click();
       cy.get('app-navbar').find('button').contains('Sign');
     });
