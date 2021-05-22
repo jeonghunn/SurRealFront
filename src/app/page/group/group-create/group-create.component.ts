6ac971f15059761368c0cddd727870cc4f705c18
Junghoon Lee <jeonghunn1@gmail.com>
2021-05-22 14:33:46 +0900

    chore: Add a group-create component

---
+++ src/app/page/group/group-create/group-create.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-group-create',
+  templateUrl: './group-create.component.html',
+  styleUrls: [ './group-create.component.scss' ],
+})
+export class GroupCreateComponent {
+
+}
