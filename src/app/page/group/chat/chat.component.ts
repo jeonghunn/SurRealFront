ec721e8a04144431aaf260c441956479f557b6ca
Junghoon Lee <jeonghunn1@gmail.com>
2021-09-22 17:40:52 +0900

    fix: Align chat input section depends on side navigation bar and window width

--- src/app/page/group/chat/chat.component.ts
+++ src/app/page/group/chat/chat.component.ts
@@ -1,14 +1,57 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  OnDestroy,
+} from '@angular/core';
+import { Subscription } from 'rxjs';
+import { LayoutService } from 'src/app/core/layout.service';
+import { Util } from 'src/app/core/util';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: [ './chat.component.scss' ],
 })
-export class ChatComponent {
+export class ChatComponent implements OnDestroy {
+
+  public DEFAULT_FOOTER_WIDTH: number = 400;
+
+  public isShortWidth: boolean = false;
+
+  private readonly subscriptions: Subscription[] = [];
+
+  public constructor(
+    private layoutService: LayoutService,
+    private changeDetectorRef: ChangeDetectorRef,
+  ) {
+    this.subscriptions = [
+      this.layoutService.windowResize$.subscribe(window => {
+        this.isShortWidth = this.layoutService.isShortWidth();
+      }),
+      this.layoutService.isSideNavOpen$.subscribe(isOpen => {
+        this.changeDetectorRef.markForCheck();
+      }),
+    ];
+  }
+
+  public get isSideNavOpen(): boolean {
+    return this.layoutService.isSideNavOpen;
+  }
+
+  public get footerWidth(): string {
+    if (this.isShortWidth) {
+      return this.isSideNavOpen ? null : `calc(100% - 16px)`;
+    }
+
+    return `${ this.DEFAULT_FOOTER_WIDTH }px`;
+  }
 
   public counter(i: number): any[] {
     return new Array(i);
+  }
+
+  public ngOnDestroy(): void {
+    Util.unsubscribe(...this.subscriptions);
   }
 
 }
