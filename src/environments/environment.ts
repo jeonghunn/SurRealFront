195b4544254fcbc2eb7f087c18995ac6a7c46b5d
Junghoon Lee <jeonghunn1@gmail.com>
2022-01-29 16:40:18 +0900

    chore: Update socketServerIUrl for production build

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  socketServerUrl: 'ws://127.0.0.1:3000',
+  socketServerUrl: 'ws://https://surreal.unopenedbox.com/api',
   api_url: '/api',
   api_key: 'xT3FP4AuctM',
   api_version: 1,
