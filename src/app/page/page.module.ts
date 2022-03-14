fc1073259ad9fbf3ad9f1e56193f6d55fb31a1f4
Junghoon Lee <jeonghunn1@gmail.com>
2022-03-15 07:16:26 +0900

    chore: Center group image vertically for mobile

--- src/app/page/page.module.ts
+++ src/app/page/page.module.ts
@@ -6,6 +6,7 @@ import {
 } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatRippleModule } from '@angular/material/core';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
@@ -68,6 +69,7 @@ import { RoomComponent } from './group/room/room.component';
         ComponentsModule,
         FormsModule,
         NgxSkeletonLoaderModule,
+        MatRippleModule,
     ],
 })
 export class PageModule { }
