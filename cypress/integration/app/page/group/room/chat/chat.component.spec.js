6dc9f945828600824f8d1f6451d907c3f0392e17
Junghoon Lee <jeonghunn1@gmail.com>
2022-05-05 19:22:46 +0900

    chore: Fix a broken tests

--- cypress/integration/app/page/group/room/chat/chat.component.spec.js
+++ cypress/integration/app/page/group/room/chat/chat.component.spec.js
@@ -9,13 +9,15 @@ context('Chat Component', () => {
 
 
     it('Type Chat Message', () => {
-      cy.get('#chat_message_input').type("Hello How are you today{enter}");
-      cy.get('.chat').find('.message').first().find('.chat').find('span').should('have.text', "Hello How are you today")
+      const timestamp = new Date().getTime();
+      cy.get('#chat_message_input').type(`Hello How are you today${timestamp}{enter}`);
+      cy.get('.chat').find('.message').last().find('.chat').find('span').should('have.text', "Hello How are you today" + timestamp)
     });
 
     it('Type Chat Message - Korean', () => {
-      cy.get('#chat_message_input').type("안녕하세요 반갑습니다{enter}");
-      cy.get('.chat').find('.message').first().find('.chat').find('span').should('have.text', "안녕하세요 반갑습니다")
+      const timestamp = new Date().getTime();
+      cy.get('#chat_message_input').type(`안녕하세요 반갑습니다${timestamp}{enter}`);
+      cy.get('.chat').find('.message').last().find('.chat').find('span').should('have.text', "안녕하세요 반갑습니다" + timestamp)
     });
 
 
