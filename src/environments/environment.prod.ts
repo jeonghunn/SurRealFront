7fbc48202feade69b13711186e30edd045915430
Junghoon Lee <jeonghunn1@gmail.com>
2022-12-03 21:00:06 +0900

    fix: Fix the production api path

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   socketServerUrl: 'wss://surreal.unopenedbox.com/api/',
-  api_url: 'api.php',
+  api_url: '/api',
   api_key: 'xT3FP4AuctM',
   api_version: 1,
 };
