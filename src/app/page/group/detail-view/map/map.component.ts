05be8402b8fa5903ed911cd89c162f85b359ed69
anakin0012 <anakin0012@gmail.com>
2023-12-14 18:15:48 +0900

    add websocket

--- src/app/page/group/detail-view/map/map.component.ts
+++ src/app/page/group/detail-view/map/map.component.ts
@@ -1,21 +1,96 @@
-import { AfterViewInit, Component, ElementRef, ViewChild, Renderer2 } from '@angular/core';
-
+import {
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+  AfterViewInit,
+} from '@angular/core';
+  import {
+    catchError,
+    delayWhen,
+    retryWhen,
+    take,
+    tap,
+    throwError,
+    timer,
+  } from 'rxjs';
+import { DataService } from 'src/app/core/data.service';
+import { RoomService } from 'src/app/core/room.service';
+import {
+  Marker,
+  Chat,
+  CommunicationResult,
+  CommunicationType,
+  LiveMessage,
+  SpaceAction,
+  SpaceActionType,
+  SpaceItem,
+} from 'src/app/model/type';
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss'],
 })
-export class MapComponent implements AfterViewInit {
+export class MapComponent implements OnInit,AfterViewInit, OnDestroy {
+  @Input()
+  public groupId: number = null;
+
+  @Input()
+  public roomId: number = null;
+
+  @Input()
+  public spaceKey: string = null;
+
+  public space: any = null;
+  public map: google.maps.Map;
+  public markers: google.maps.Marker[] = [];
+  public currentInfoWindow: google.maps.InfoWindow;
+  public title: string = null;
+  public content: Marker[] = [];
+  public data: SpaceItem[] = [];
+  public isConnected: boolean = false;
+  public reconnectDelay: number = 5000;
+  public isAuthenticated: boolean = false;
+  public reAuthDelay: number = 100;
+  public isAllowEdit: boolean = false;
+
+  public subscriptions: any = [];
+  public updateTimerSubscription: any = null;
   @ViewChild('mapContainer') mapContainer: ElementRef;
   @ViewChild('pacInput') pacInput: ElementRef;
-
-  private map: google.maps.Map;
-  private markers: google.maps.Marker[] = [];
-  private currentInfoWindow: google.maps.InfoWindow;
+  public constructor(
+    private dataService: DataService,
+    private roomService: RoomService,
+    private changeDetectorRef: ChangeDetectorRef,
+  ) {}
   
-  constructor(private renderer: Renderer2) {}
-  public ngAfterViewInit(): void {
+  public ngOnInit(): void {
+    this.load();
+    this.roomService.initSpaceWebSocket(this.roomId, this.spaceKey);
+    this.subscriptions = [
+      this.roomService.spaceWebSocketSubject$.pipe(
+        retryWhen(errors =>
+          errors.pipe(
+            tap(val => this.onConnectionError(val)),
+            delayWhen(val => timer(this.reconnectDelay)),
+          ),
+        ),
+      ).subscribe(
+        (msg: any) => this.onMessageReceived(msg),
+        (err) => this.onConnectionError(err),
+        () => this.onConnectionComplete(),
+      ),
+    ];
+  }
+  public ngAfterViewInit(): void{
     this.initMap();
+  }
+  public ngOnDestroy(): void {
+    this.roomService.closeSpaceWebSocket();
   }
 
   public initMap(): void {
@@ -23,9 +98,9 @@ export class MapComponent implements AfterViewInit {
       zoom: 17,
       center: { lat: 35.88979960417728, lng:128.6101853686389},
     };
+    console.log(this.mapContainer);
 
     this.map = new google.maps.Map(this.mapContainer.nativeElement, mapOptions);
-    this.renderer.appendChild(this.mapContainer.nativeElement, this.pacInput.nativeElement);
 
     const searchBox = new google.maps.places.SearchBox(this.pacInput.nativeElement);
     this.map.controls[google.maps.ControlPosition.TOP_CENTER].push(this.pacInput.nativeElement);
@@ -138,9 +213,9 @@ export class MapComponent implements AfterViewInit {
     });
 
     this.markers.push(newMarker);
-
+    this.updateLazy();
     const infowindow = new google.maps.InfoWindow({
-      content: `<p>Marker Location: ${newMarker.getPosition()}</p>`,
+      content: `<p>Marker Location: ${newMarker.getTitle()}</p>`,
       maxWidth: 200,
     });
 
@@ -152,6 +227,31 @@ export class MapComponent implements AfterViewInit {
       this.removeMarker(newMarker);
     });
   }
+  private loadMarkers(markers: Marker[]) {
+    markers.map(marker => {
+      const newMarker = new google.maps.Marker({
+        position: { lat: marker.lat, lng: marker.lng },
+        map: this.map,
+        title: marker.title,
+      });
+  
+      this.markers.push(newMarker);
+  
+      const infowindow = new google.maps.InfoWindow({
+        content: `<p>${newMarker.getTitle()}</p>`,
+        maxWidth: 200,
+      });
+  
+      newMarker.addListener('click', () => {
+        infowindow.open(this.map, newMarker);
+      });
+  
+      newMarker.addListener('rightclick', () => {
+        this.removeMarker(newMarker);
+      });
+    });
+  }
+  
 
   private removeMarker(marker: google.maps.Marker): void {
     marker.setMap(null);
@@ -159,6 +259,98 @@ export class MapComponent implements AfterViewInit {
     const index = this.markers.indexOf(marker);
     if (index !== -1) {
       this.markers.splice(index, 1);
+    }
+  }
+  public onMessageReceived(msg: any): void {
+
+    switch (msg.T) {
+      case CommunicationType.AUTH:
+        const authResult: CommunicationResult = msg as CommunicationResult;
+        this.roomService.onAuthResultReceived(authResult.result);
+      case CommunicationType.LIVE:
+        const version: number = parseInt(msg.content || 0);
+        
+        if (version <= this.space?.version) {
+          return;
+        }
+
+        this.load();
+
+        break;
+    }
+  }
+
+
+  public load(): void {
+    this.dataService.getSpace(this.groupId, this.roomId, this.spaceKey).subscribe((space) => {
+      if (space.updatedAt === this.space?.updatedAt) {
+        return;
+      }
+
+      // this.isEditMode = space.isEditMode;
+      this.space = space;
+      this.title = space.title;
+      this.content = space.content ||[];
+      this.loadMarkers(JSON.parse(space.content));
+     });
+  }
+
+  public update(): void {
+    this.mapping();
+    console.log(this.markers);
+    console.log(this.content);
+    console.log(this.space)
+    this.dataService.updateSpace(
+      this.space?.key,
+      this.groupId,
+      this.roomId,
+      this.title,
+      JSON.stringify(this.content),
+      this.space?.version,
+      ).pipe(
+        take(1),
+        catchError((err) => {
+         if (err?.status === 406) {
+          this.load();
+        }
+        return throwError(err);
+      }),
+      ).subscribe((space) => {
+      this.space = space;
+      });
+    }
+
+    public updateLazy(): void {
+      if (this.updateTimerSubscription){
+        this.updateTimerSubscription.unsubscribe();
+      }
+  
+      this.updateTimerSubscription = timer(1000).pipe(take(1)).subscribe(() => {
+        this.update();
+      });
+    }
+  public mapping(): void{
+    this.content=this.markers.map((item: any) => {
+      const position = item.getPosition();
+      let lat:number = position.lat();
+      let lng:number = position.lng();
+      const result: Marker = new Marker(item.getTitle(),lat,lng);
+      return result;
+    });
+  }
+
+  public onConnectionError(error: any = null): void {
+    console.log('ConnectionError', error);
+    this.isConnected = false;
+  }
+
+  public onConnectionComplete(): void {
+    console.log('docuent socket complete');
+    this.isAllowEdit = true;
+    this.changeDetectorRef.markForCheck();
+
+    if (!this.isConnected) {
+      this.load();
     }
   }
 }
