a40fd425fc5a5096fd5072a0c3b1915b540910fc
Junghoon Lee <jeonghunn1@gmail.com>
2025-02-02 17:54:51 +0900

    refactor: Make the spinner component standalone

--- src/ui/ui.module.ts
+++ src/ui/ui.module.ts
@@ -4,12 +4,10 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { TranslateModule } from '@ngx-translate/core';
 import { BigerrorComponent } from './bigerror/bigerror.component';
-import { SpinnerComponent } from './spinner/spinner.component';
 
 @NgModule({
   declarations: [
     BigerrorComponent,
-    SpinnerComponent,
   ],
   imports: [
     CommonModule,
@@ -19,7 +17,6 @@ import { SpinnerComponent } from './spinner/spinner.component';
   ],
   exports: [
     BigerrorComponent,
-    SpinnerComponent,
   ],
 })
 export class UiModule { }
